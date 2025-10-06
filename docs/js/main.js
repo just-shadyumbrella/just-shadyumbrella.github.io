de438461bb0e23af8ec751cebb1f2255487e6c2b
just-shadyumbrella <xshadyumbrella@proton.me>
2025-10-06 19:57:17 +0700

    Update

---
+++ docs/js/main.js
@@ -0,0 +1,182 @@
+;(function () {
+  'use strict'
+
+  var isMobile = {
+    Android: function () {
+      return navigator.userAgent.match(/Android/i)
+    },
+    BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i)
+    },
+    iOS: function () {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i)
+    },
+    Opera: function () {
+      return navigator.userAgent.match(/Opera Mini/i)
+    },
+    Windows: function () {
+      return navigator.userAgent.match(/IEMobile/i)
+    },
+    any: function () {
+      return (
+        isMobile.Android() ||
+        isMobile.BlackBerry() ||
+        isMobile.iOS() ||
+        isMobile.Opera() ||
+        isMobile.Windows()
+      )
+    },
+  }
+
+  var fullHeight = function () {
+    if (!isMobile.any()) {
+      $('.js-fullheight').css('height', $(window).height())
+      $(window).resize(function () {
+        $('.js-fullheight').css('height', $(window).height())
+      })
+    }
+  }
+
+  // Parallax
+  var parallax = function () {
+    $(window).stellar()
+  }
+
+  var contentWayPoint = function () {
+    var i = 0
+    $('.animate-box').waypoint(
+      function (direction) {
+        if (direction === 'down' && !$(this.element).hasClass('animated-fast')) {
+          i++
+
+          $(this.element).addClass('item-animate')
+          setTimeout(function () {
+            $('body .animate-box.item-animate').each(function (k) {
+              var el = $(this)
+              setTimeout(
+                function () {
+                  var effect = el.data('animate-effect')
+                  if (effect === 'fadeIn') {
+                    el.addClass('fadeIn animated-fast')
+                  } else if (effect === 'fadeInLeft') {
+                    el.addClass('fadeInLeft animated-fast')
+                  } else if (effect === 'fadeInRight') {
+                    el.addClass('fadeInRight animated-fast')
+                  } else {
+                    el.addClass('fadeInUp animated-fast')
+                  }
+
+                  el.removeClass('item-animate')
+                },
+                k * 100,
+                'easeInOutExpo'
+              )
+            })
+          }, 50)
+        }
+      },
+      { offset: '85%' }
+    )
+  }
+
+  var goToTop = function () {
+    $('.js-gotop').on('click', function (event) {
+      event.preventDefault()
+
+      $('html, body').animate(
+        {
+          scrollTop: $('html').offset().top,
+        },
+        500,
+        'easeInOutExpo'
+      )
+
+      return false
+    })
+
+    $(window).scroll(function () {
+      var $win = $(window)
+      if ($win.scrollTop() > 200) {
+        $('.js-top').addClass('active')
+      } else {
+        $('.js-top').removeClass('active')
+      }
+    })
+  }
+
+  var pieChart = function () {
+    $('.chart').easyPieChart({
+      scaleColor: false,
+      lineWidth: 4,
+      lineCap: 'butt',
+      barColor: '#FF9000',
+      trackColor: '#f5f5f5',
+      size: 160,
+      animate: 1000,
+    })
+  }
+
+  var skillsWayPoint = function () {
+    if ($('#fh5co-skills').length > 0) {
+      $('#fh5co-skills').waypoint(
+        function (direction) {
+          if (direction === 'down' && !$(this.element).hasClass('animated')) {
+            setTimeout(pieChart, 400)
+            $(this.element).addClass('animated')
+          }
+        },
+        { offset: '90%' }
+      )
+    }
+  }
+
+  // Loading page
+  var loaderPage = function () {
+    $('.fh5co-loader').fadeOut('slow')
+  }
+
+  var simpleIconsLoader = function () {
+    $('i[class^="si-"]').each(function () {
+      var $icon = $(this)
+      var classes = $icon.attr('class').split(' ')
+
+      for (var i = 0; i < classes.length; i++) {
+        if (classes[i].startsWith('si-')) {
+          var parts = classes[i].split('-')
+          if (parts.length >= 4) {
+            var size = parts[1]
+            var slug = parts[2]
+            var color = parts[3]
+            var darkModeColor = parts[4] || ''
+
+            var imgSrc = 'https://cdn.simpleicons.org/' + slug + '/' + color
+            if (darkModeColor) {
+              imgSrc += '/' + darkModeColor
+            }
+
+            var $img = $('<img>', {
+              class: 'simple-icons',
+              height: size,
+              width: size,
+              src: imgSrc,
+            })
+
+            $icon.replaceWith($img)
+            break
+          }
+        }
+      }
+    })
+  }
+
+  $(function () {
+    contentWayPoint()
+    goToTop()
+    loaderPage()
+    fullHeight()
+    parallax()
+    // pieChart()
+    skillsWayPoint()
+    simpleIconsLoader()
+  })
+})()
